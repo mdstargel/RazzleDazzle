c7b595444992035b028ebb4eb54b1dcd9c40f363
rbsquires <89427939+rbsquires@users.noreply.github.com>
2022-10-23 17:35:25 -0400

    Banner Changes
    Made changes to main banner, About Us/Services/News

--- my-app/src/Components/Banner/Banner.jsx
+++ my-app/src/Components/Banner/Banner.jsx
@@ -8,10 +8,12 @@ const Banner = ({ setwpage, wpage, signedIn, userPermissions, setSignedIn }) => 
     const defaultbanner = (
         <div className="banner">
             <div className="tab" style={{left: '0px'}}>Logo Here</div>
-            <div className="tab services"
-                onClick={() => setwpage('Services')}>Services</div>
-            <div className="tab aboutus" onClick={() => setwpage('About Us')}>About Us</div>
-            <div className="tab news" onClick={() => setwpage('News')}>News</div>
+                <div className='centerTabs'>
+                    <div className="tab services"
+                        onClick={() => setwpage('Services')}>Services</div>
+                    <div className="tab aboutus" onClick={() => setwpage('About Us')}>About Us</div>
+                    <div className="tab news" onClick={() => setwpage('News')}>News</div>
+                </div>
         <div className="loginTab">
             <div className="login" onClick={() => setwpage('Log In')}>Log In</div>
             <div className="signup" onClick={() => setwpage('Sign Up')}>Sign Up</div>
