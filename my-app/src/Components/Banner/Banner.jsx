f491682d97a553d3a7cacc045386b029636dd75d
rbsquires <rbsquires@csustudent.net>
2022-11-06 11:19:17 -0500

    merging
    merging

--- my-app/src/Components/Banner/Banner.jsx
+++ my-app/src/Components/Banner/Banner.jsx
@@ -13,14 +13,22 @@ const Banner = ({ setwpage, wpage, signedIn, userPermissions, setSignedIn }) => 
     const defaultbanner = (
         <div className="banner">
             <div className="tab" style={{ left: '0px' }}>Logo Here</div>
-            <div 
+            <div
                 className={`tab services ${wpage === "Services" ? 'activetab' : ''}`}
-                    onClick={() => setwpage('Services')} >Services</div>
-                    <div className="tab aboutus" onClick={() => setwpage('About Us')}>About Us</div>
-                    <div className="tab news" onClick={() => setwpage('News')}>News</div>
+                        onClick={() => setwpage('Services')} >Services</div>
+            <div
+                className={`tab aboutus ${wpage === "About Us" ? 'activetab' : ''}`}
+                onClick={() => setwpage('About Us')}>About Us</div>
+            <div
+                className={`tab news ${wpage === "News" ? 'activetab' : ''}`}
+                onClick={() => setwpage('News')}>News</div>
             <div className="loginTab">
-                <div className="login" onClick={() => setwpage('Log In')}>Log In</div>
-                <div className="signup" onClick={() => setwpage('Sign Up')}>Sign Up</div>
+                <div
+                    className={`login ${wpage === "Log In" ? 'activetab' : ''}`}
+                    onClick={() => setwpage('Log In')}>Log In</div>
+                <div
+                    className={`signup ${wpage === "Sign Up" ? 'activetab' : ''}`}
+                    onClick={() => setwpage('Sign Up')}>Sign Up</div>
             </div>
         </div>
     );
