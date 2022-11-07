06b9166912052010a9d649d54e7e4da9a46c874d
JoeKauf <josephandrewkaufman@gmail.com>
2022-11-06 22:01:41 -0500

    News Additions

--- my-app/src/Components/User Pages/AvailableAppointments.jsx
+++ my-app/src/Components/User Pages/AvailableAppointments.jsx
@@ -12,7 +12,7 @@ const AvailableAppointments = ({ availableAppointments, userPermissions }) => {
     const pills = trainersList.map(trainerName => <div> <AppointmentPill trainerName={trainerName} availableAppointments={availableAppointments} userPermissions={userPermissions}/></div> );
 
     return (
-        <div>{pills}</div>
+        <div style={{display: 'flex'}}>{pills}</div>
     )
 }
 
