0cc650e29ed4b2564ab85c0b3ddda28d89641ec8
JoeKauf <josephandrewkaufman@gmail.com>
2022-11-07 01:02:57 -0500

    Form on Calendar modified

--- my-app/src/Components/Buttons/CalendarAddButton.jsx
+++ my-app/src/Components/Buttons/CalendarAddButton.jsx
@@ -5,8 +5,6 @@ const CalendarAddButton = ({ onClick }) => {
         <input type='button' value='Add' onClick={onClick} style={{
             backgroundColor: '#0C1526',
             color: 'White',
-            // width: '140px',
-            // height: '45px',
             border: '1px solid rgba(0, 0, 0, 0.2)',
             boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
             borderRadius: '10px',
